01f55f10c8f4edbf474964dd797b9a3755f796ec
ArtyGeneralov <artyom1100@gmail.com>
2023-08-28 00:08:41 +0300

    cleanup

--- Ex03.ConsoleUI/Utils/EMainMenuItems.cs
+++ Ex03.ConsoleUI/Utils/EMainMenuItems.cs
@@ -4,11 +4,6 @@ namespace Ex03.ConsoleUI
 {
     enum EMainMenuItems
     {
-        //main menu -> 1. add vehicle to garage
-        //              2. show licenseplate list
-        //              2.1 by EVehileStatus
-        //              2.2 default order
-        //              3. quit
         AddVehicleToGarage,
         ShowVehiclesInGarage,
         ChangeVehicleState,
